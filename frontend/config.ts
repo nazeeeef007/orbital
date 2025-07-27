7652815364189b2c4a4ade586d4662e4de749d66
nazeef <muhdnazeef007@gmail.com>
2025-07-27 11:10:07 +0800

    idk man

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,5 @@
 // export const BASE_URL = "http://192.168.68.101:3000";
-export const BASE_URL = "https://orbital-ms3a.onrender.com";
-// export const BASE_URL = "https://prata-backend.onrender.com";
+import Constants from 'expo-constants';
+
+export const BASE_URL = Constants.expoConfig?.extra?.BASE_URL;
+// (or Constants.manifest?.extra?.BASE_URL in older SDKs)
